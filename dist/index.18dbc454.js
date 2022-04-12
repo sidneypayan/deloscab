92ae0b9f015b7407faedd5c469d7d737b0c0b685
sidney <etreailleurs@hotmail.fr>
2022-04-12 15:38:38 +0200

    form done

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -522,17 +522,38 @@ var _form = require("./form"); // import('./form.js').then(form => {
  // })
 
 },{"./form":"l9hyy"}],"l9hyy":[function(require,module,exports) {
-// export function render() {
-const formDevis = document.querySelector('#formDevis'), depart = document.querySelector('#depart'), arrivee = document.querySelector('#arrivee'), passagers = document.querySelector('#passagers'), date = document.querySelector('#date'), heure = document.querySelector('#heure'), nom = document.querySelector('#nom'), telephone = document.querySelector('#telephone'), email = document.querySelector('#email'), demande = document.querySelector('#demande'), error = document.querySelector('#error');
+const formDevis = document.querySelector('#formDevis'), depart = document.querySelector('#depart'), arrivee = document.querySelector('#arrivee'), passagers = document.querySelector('#passagers'), date = document.querySelector('#date'), heure = document.querySelector('#heure'), nom = document.querySelector('#nom'), telephone = document.querySelector('#telephone'), email = document.querySelector('#email'), message = document.querySelector('#message'), error = document.querySelector('#error'), success = document.querySelector('#success');
 const checkForm = (e)=>{
     if (!depart.value || !arrivee.value || !passagers.value || !date.value || !heure.value || !nom.value || !telephone.value || !email.value) {
         e.preventDefault();
         error.style.display = 'block';
-        error.innerText = 'Veuillez remplir tous les champs du formulaire';
+    } else {
+        e.preventDefault();
+        formDevis.style.display = 'none';
+        success.style.display = 'block';
+        console.log(depart.value, arrivee.value, passagers.value, date.value, heure.value, nom.value, telephone.value, email.value, message.value);
+        Email.send({
+            Host: 'smtp.ionos.fr',
+            Username: 'contact@deloscab.com',
+            Password: '&8zAu8u8BUWQdm9',
+            To: 'contact@deloscab.com',
+            From: email.value,
+            Subject: 'Demande de devis | réservation',
+            Body: `
+      		  Départ : ${depart.value}
+      		  Arrivée : ${arrivee.value}
+      		  Nombre de passagers : ${passagers.value}
+      		  Date : ${date.value}
+      		  Heure : ${heure.value}
+      		  Nom : ${nom.value}
+      		  Téléphone : ${telephone.value}
+      		  Email : ${email.value}
+      		  Message : ${message.value}
+      		  `
+        });
     }
 };
-formDevis.addEventListener('submit', checkForm) // }
-;
+formDevis.addEventListener('submit', checkForm);
 
 },{}]},["g9TDx","1SICI"], "1SICI", "parcelRequireb988")
 
--- src/js/form.js
+++ src/js/form.js
@@ -1,4 +1,3 @@
-// export function render() {
 const formDevis = document.querySelector('#formDevis'),
 	depart = document.querySelector('#depart'),
 	arrivee = document.querySelector('#arrivee'),
@@ -8,8 +7,9 @@ const formDevis = document.querySelector('#formDevis'),
 	nom = document.querySelector('#nom'),
 	telephone = document.querySelector('#telephone'),
 	email = document.querySelector('#email'),
-	demande = document.querySelector('#demande'),
-	error = document.querySelector('#error')
+	message = document.querySelector('#message'),
+	error = document.querySelector('#error'),
+	success = document.querySelector('#success')
 
 const checkForm = e => {
 	if (
@@ -24,9 +24,41 @@ const checkForm = e => {
 	) {
 		e.preventDefault()
 		error.style.display = 'block'
-		error.innerText = 'Veuillez remplir tous les champs du formulaire'
+	} else {
+		e.preventDefault()
+		formDevis.style.display = 'none'
+		success.style.display = 'block'
+		console.log(
+			depart.value,
+			arrivee.value,
+			passagers.value,
+			date.value,
+			heure.value,
+			nom.value,
+			telephone.value,
+			email.value,
+			message.value
+		)
+		Email.send({
+			Host: 'smtp.ionos.fr',
+			Username: 'contact@deloscab.com',
+			Password: '&8zAu8u8BUWQdm9',
+			To: 'contact@deloscab.com',
+			From: email.value,
+			Subject: 'Demande de devis | réservation',
+			Body: `
+      		  Départ : ${depart.value}
+      		  Arrivée : ${arrivee.value}
+      		  Nombre de passagers : ${passagers.value}
+      		  Date : ${date.value}
+      		  Heure : ${heure.value}
+      		  Nom : ${nom.value}
+      		  Téléphone : ${telephone.value}
+      		  Email : ${email.value}
+      		  Message : ${message.value}
+      		  `,
+		})
 	}
 }
 
 formDevis.addEventListener('submit', checkForm)
-// }
