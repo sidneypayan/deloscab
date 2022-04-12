e50da57b593949199e56b6f57040b885405ce357
sidney <etreailleurs@hotmail.fr>
2022-04-12 15:40:56 +0200

    pre build save

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,2 @@
 import * as form from './form'
-
-// import('./form.js').then(form => {
-// 	form.render()
-// })
-
-// import('./avis.js').then(avis => {
-// 	avis.render()
-// })
+import * as swiper from './swiper'
